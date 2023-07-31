b55a374b3e81df95e887e9ce0988bf56a0868707
Westender <javaxwestender@gmail.com>
2023-07-31 14:51:03 +0300

    Add: task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
-    public static void main(String[] args) {
 
+    static int counter = 1;
+
+    public static void main(String[] args) {
+        task1();
     }
+
+    static void task1() {
+        System.out.printf("\nЗадание %d:", counter++);
+
+        int[] intArray = new int[3];
+
+        for (int i : intArray) {
+            intArray[i] = i + 1;
+        }
+
+        double[] doubleArray = {1.57, 7.654, 9.986};
+
+        String str = "Hello";
+
+        char[] charArray = str.toCharArray();
+    }
+
 }
